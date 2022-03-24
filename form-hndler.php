3ffe3718c4c2c58e4854b362a59a841c0a7ca15a
M_H_MISU <83594748+Webdevelopermisu@users.noreply.github.com>
2022-03-24 23:40:27 +0600

    Update form-hndler.php

--- form-hndler.php
+++ form-hndler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'satroligb@gmail.com';
 
 $email_subject = 'New Form Submission';
 
